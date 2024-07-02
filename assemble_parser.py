b770647f670dead83cd4c5ee4b901e8fcb5134cb
JoeBai3r <jbaier@jbaier>
2024-07-02 09:49:39 -0400

    reworked code with classes in functions, now can take multiple file inputes

--- assemble_parser.py
+++ assemble_parser.py
@@ -5,33 +5,96 @@ import csv
 import os
 import json
 
-#read in json file
-json_file = sys.argv[1]
+#class File stores each file from json
+class File:
 
-with open(json_file, 'r') as j_file:
-    json_data = json.load(j_file)
+    def __init__(self, filename, counter_arr):
+        self.filename = filename
+        self.counter_arr = counter_arr
 
-print(json_data)
 
-input_program = json_data["input_program"]
-directory = json_data["or1k_directory"]
-counter_number = json_data["counter_number"]
+#class to store each counter
+class Counter:
 
-counter_arr = []
-for i in range(1, counter_number+1):
-    key = f'counter_{i}'
-    counter_arr.append(json_data[key])
+    def __init__(self, name, component_to_track, increment, weight):
+        self.name = name
+        self.component_to_track = component_to_track
+        self.increment = increment
+        self.weight = weight
+        self.count = 0
 
-print(counter_arr)
-#use run input program on or1k
-subprocess.run(['or1k-elf-gcc', input_program, '-o', 'output'], cwd=directory, text=True)
 
-#dump to output file
-objdump_command = ['or1k-elf-objdump', '-d', 'output']
-output_file_path = os.path.join(directory, 'new_output.txt')
+def generate_assemble(new_program):
+    #use run input program on or1k
+    subprocess.run(['or1k-elf-gcc', new_program, '-o', 'output'], cwd=directory, text=True)
 
-with open(output_file_path, 'w') as output_file:
-    subprocess.run(objdump_command, cwd=directory, check=True, text=True, stdout=output_file)
+    #dump to output file
+    objdump_command = ['or1k-elf-objdump', '-d', 'output']
+    output_file_path = os.path.join(directory, 'new_output.txt')
+
+    with open(output_file_path, 'w') as output_file:
+        subprocess.run(objdump_command, cwd=directory, check=True, text=True, stdout=output_file)
+
+    return output_file_path
+
+
+def generate_counter_dict(output_file_path):
+    #create counter dict for all commands
+    counter_dict = {}
+    with open(output_file_path, 'r') as file:
+
+        record = False
+        stop = False
+        for line in file:
+            
+            words = line.split()
+            
+            for word in words:
+                if word == '<main>:':
+                    record = True
+                elif (word.startswith('l.') or word.startswith('lf.') or word.startswith('lv.')) and record:
+                    if word in counter_dict:
+                        counter_dict[word] += 1
+                    else:
+                        counter_dict[word] = 1
+                elif word == '<atexit>:':
+                    stop = True
+                    break
+            if stop:
+                break
+    return counter_dict
+
+
+def update_counts(counter_dict):
+
+    for key, value in counter_dict.items():
+        for i in range(value):
+            #regular arithmetic or logical command
+            if commands_dict[key] == 1:
+                component_dict["instruction_fetch"] += 1
+                component_dict["instruction_decode"] += 1
+                component_dict["ALU"] += 1
+            
+            #branch command
+            elif commands_dict[key] == 2:
+                component_dict["program_counter"] += 1
+                component_dict["instruction_fetch"] += 1
+                component_dict["instruction_decode"] += 1
+                component_dict["ALU"] += 1
+
+            #jump command
+            elif commands_dict[key] == 3:
+                component_dict["program_counter"] += 1
+                component_dict["instruction_fetch"] += 1
+                component_dict["instruction_decode"] += 1
+
+            #load and store commands
+            elif commands_dict[key] == 4:
+                component_dict["program_counter"] += 1
+                component_dict["instruction_fetch"] += 1
+                component_dict["instruction_decode"] += 1
+                component_dict["memory_management_unit"] += 1
+
 
 commands_dict = {
     "l.add": 1, "l.addi": 1, "l.addc": 1,"l.addic": 1,"l.adrp": 1, "l.sub": 1, "l.cmov": 0,"l.muli": 1, "l.mul": 1,"l.mulu": 1,"l.muld": 1,"l.muldu": 1, "l.div": 1, "l.divu": 1, "rem": 0, "remu": 0,
@@ -39,75 +102,55 @@ commands_dict = {
     "l.lb": 4, "l.lbu": 4, "l.lh": 4, "l.lhz": 4, "l.lhu": 4, "l.lhs": 4, "l.lw": 4,"l.lwa": 4,"l.lws": 4,"l.lwz": 4, "l.ld": 4, "l.lf": 4, 
     "l.sb": 4, "l.sh": 4, "l.sw": 4,"l.swa": 4, "l.sd": 4, "l.movhi": 0,"l.msb": 0,"l.msbu": 0, "l.ror": 0,"l.rori": 0,
     "l.extbs": 0,"l.extbz": 0,"l.exths": 0,"l.exthz": 0,"l.extws": 0,"l.extwz": 0,"l.ff1": 0,"l.fl1": 0,
-    "l.j": 3, "l.jal": 3, "l.jr": 3, "l.jalr": 3, "l.bf": 2, "l.bne": 2, "l.nop": 0, "l.mtspr": 0,"l.rfe": 0, "l.sys": 0, "l.trap": 0, "l.msync": 0,"l.csync": 0, "l.psync": 0,
+    "l.j": 3, "l.jal": 3, "l.jr": 3, "l.jalr": 3,"l.bnf": 2, "l.bf": 2, "l.bne": 2, "l.nop": 0, "l.mtspr": 0,"l.rfe": 0, "l.sys": 0, "l.trap": 0, "l.msync": 0,"l.csync": 0, "l.psync": 0,
     "l.cust1": 0,"l.cust2": 0,"l.cust3": 0,"l.cust4": 0,"l.cust5": 0,"l.cust6": 0,"l.cust7": 0,"l.cust8": 0,"l.sflts": 0,"l.sfltsi": 0,"l.sfltu": 0,"l.sfltui": 0,
     "lf.add.s": 0, "lf.sub.s": 0, "lf.mul.s": 0, "lf.div.s": 0, "lf.sqrt.s": 0, "lf.madd.s": 0, "lf.cmp.s": 0,"l.sfles": 0,"l.sflesi": 0,"l.sfleu": 0,"l.sfleui": 0,
     "l.mac": 0,"l.maci": 0,"l.macrc": 0,"l.macu": 0,"l.mfspr": 0,"l.sfges": 0, "l.sfgesi": 0,"l.sfgeu": 0,"l.sfgeui": 0,"l.sfgts": 0,"l.sfgtsi": 0,"l.sfgtu": 0,"l.sfgtui": 0,
     "lf.itof.s": 0, "lf.ftoi.s": 0, "lf.mov.s": 0, "l.lbz": 0, "l.sfne": 0,"l.sfnei": 0, "l.sfeq": 0,"l.sfeqi": 0, "lwz": 0,"l.lbs": 0
 }
 
-#create counter dict for all commands
-counter_dict = {}
 
-with open(output_file_path, 'r') as file:
-    record = False
-    stop = False
-    for line in file:
-        
-        words = line.split()
-        
-        for word in words:
-            if word == '<main>:':
-                record = True
-            elif (word.startswith('l.') or word.startswith('lf.') or word.startswith('lv.')) and record:
-                if word in counter_dict:
-                    counter_dict[word] += 1
-                else:
-                    counter_dict[word] = 1
-            elif word == '<atexit>:':
-                stop = True
-                break
-        if stop:
-            break
+#read in json file
+json_file = sys.argv[1]
 
-for key in counter_dict:
-    print(f'{key} : {counter_dict[key]}')
+with open(json_file, 'r') as j_file:
+    json_data = json.load(j_file)
 
-component_dict = {"Instruction_Fetch": 0, "Instruction_Decode": 0, "ALU": 0, "Program_Counter": 0 }
+input_programs = json_data["input_programs"]
+directory = json_data["or1k_directory"]
+counter_number = json_data["counter_number"]
+
+#create file to store file objects
+file_arr = []
+
+#iterate through all programs 
+for i in input_programs:
+
+    #create counters for specific file
+    counter_arr = []
+    for j in range(1, counter_number+1):
+        key = f'counter_{j}'
+        counter = json_data[key]
+        counter_arr.append(Counter(counter["name"], counter["component_to_track"], counter["increment_value"], counter["weight"]))
+
+    component_dict = {"instruction_fetch": 0, "instruction_decode": 0, "ALU": 0, "program_counter": 0, "memory_management_unit": 0}
+    output_file = generate_assemble(i)
+    counter_dict = generate_counter_dict(output_file)
+    update_counts(counter_dict)
+
+    for j in range(len(counter_arr)):
+        counter_arr[j].count = component_dict[counter_arr[j].component_to_track]
+
+    file_arr.append(File(i, counter_arr))
 
 
-for key, value in counter_dict.items():
-    for i in range(value):
-        #regular arithmetic or logical command
-        if commands_dict[key] == 1:
-            component_dict["Instruction_Fetch"] += 1
-            component_dict["Instruction_Decode"] += 1
-            component_dict["ALU"] += 1
-        
-        #branch command
-        elif commands_dict[key] == 2:
-            component_dict["Program_Counter"] += 1
-            component_dict["Instruction_Fetch"] += 1
-            component_dict["Instruction_Decode"] += 1
-            component_dict["ALU"] += 1
+    
+for i in file_arr:
+    print(i.filename)
+    for j in i.counter_arr:
+        print(j.name, ":", j.count)
 
-        #jump command
-        elif commands_dict[key] == 3:
-            component_dict["Program_Counter"] += 1
-            component_dict["Instruction_Fetch"] += 1
-            component_dict["Instruction_Decode"] += 1
-
-        #load and store commands
-        elif commands_dict[key] == 4:
-            component_dict["Program_Counter"] += 1
-            component_dict["Instruction_Fetch"] += 1
-            component_dict["Instruction_Decode"] += 1
-            
-for key in component_dict:
-    print(f'{key} : {component_dict[key]}')
 ''''
-basic_dict = {"Program": input_program, "Line Count": line_count, "Constant Variable": variables, "Branches": branch, "Jumps": jumps}
-
 csv_file = 'new_assemble_out.csv'
 
 file_exists = os.path.isfile(csv_file)
@@ -132,6 +175,7 @@ with open(csv_file, mode='a' if file_exists else 'w', newline='') as file:
     combined_dict = {**basic_dict, **commands_dict}
     writer.writerow(combined_dict)
 
+
 for key in commands_dict:
     pattern = re.compile(rf'\b{key}\b')
     matches = pattern.findall(assemble_output.stdout)
@@ -155,5 +199,3 @@ for key in commands_dict:
         writer.writerows(rows)
 
 '''
-
-
